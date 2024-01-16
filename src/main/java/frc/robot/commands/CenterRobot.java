24e1095b2e3737ff5a556abfa6bfa304786a6f7f
Vimalnath <vimalnathsh76@gmail.com>
2024-01-15 21:11:45 -0500

    First Commit, Untested Vision System

---
+++ src/main/java/frc/robot/commands/CenterRobot.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class CenterRobot extends Command {
+  /** Creates a new CenterRobot. */
+  DriveSubsystem driveTrain;
+  private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
+  private PIDController turnpid;
+  double kPThetaController=1;
+  double kIThetaController=0;
+  double kDThetaController=0;
+  public CenterRobot(DriveSubsystem subsystem) {
+    driveTrain=subsystem;
+    addRequirements(driveTrain);
+    
+    turnpid = new PIDController(kPThetaController, kIThetaController, kDThetaController);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double angle=Math.toRadians(m_gyro.getAngle(ADIS16470_IMU.IMUAxis.kYaw)*(Math.PI/180));
+    double speed=turnpid.calculate(angle);
+    driveTrain.drive(0, 0, speed, false, true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/commands/LineUptoTag.java
@@ -0,0 +1,89 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.*;
+
+public class LineUptoTag extends Command {
+  /** Creates a new LineUptoTag. */
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  DriveSubsystem driveTrain;
+  NetworkTableEntry tx=table.getEntry("tx");
+  NetworkTableEntry ty=table.getEntry("ty");
+  double xoffset;
+  double yOffset;
+  double xKp = 0.1; //Subject to change
+  double xKi=0;
+  double xKd=0;
+  double xSpeed;
+  double zdistancefromtag;
+  double xdistancefromtag;
+  private PIDController xcontroller;
+
+  public LineUptoTag(DriveSubsystem subsystem) {
+    driveTrain=subsystem;
+    addRequirements(driveTrain);
+    xcontroller=new PIDController(xKp,xKi,xKd);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // CenterRobot(driveTrain);
+    Update_Limelight();
+  }
+
+  public void Update_Limelight(){
+    double tv=table.getEntry("tv").getDouble(0.0);
+    if (tv!=0){
+      xoffset=tx.getDouble(0);
+      yOffset=ty.getDouble(0);
+      zdistancefromtag=getDistancefromTag(7.626, 48.125, 10, yOffset);//in inches, subject to change
+      xdistancefromtag=zdistancefromtag*Math.tan(Math.toRadians(xoffset));
+      SmartDashboard.putNumber("LimelightX", xoffset);
+      SmartDashboard.putNumber("Z-distance", zdistancefromtag);
+      SmartDashboard.putNumber("X-distance", xdistancefromtag);
+      
+
+      if (xoffset>1 || xoffset<-1){
+      xSpeed=xcontroller.calculate(xoffset);
+      SmartDashboard.putNumber("Speed", xSpeed);
+      }
+      else{
+        xSpeed=0;
+      }
+      //driveTrain.drive(0, -xSpeed, 0, false,true);
+    }
+  }
+  public double getDistancefromTag(double heightOfLimelight, double heightOfGoal, double angleOfLimelight,double yOffset){
+    yOffset+=angleOfLimelight;
+    yOffset=Math.toRadians(yOffset);
+    
+    return (heightOfGoal-heightOfLimelight)/Math.tan(yOffset);
+  }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.drive(0, 0, 0, false,true);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
